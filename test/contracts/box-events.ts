102498389f74fb81202ca30229a6e0cc2e528e1d
john <62711267+johnfr14@users.noreply.github.com>
2022-03-23 10:45:38 +0100

    Initial commit

---
+++ test/contracts/box-events.ts
@@ -0,0 +1,21 @@
+import {Event} from 'ethers'
+import {StoreEvent} from '../../typechain-types/Box'
+import {expect} from 'chai'
+
+export type ActualStoreEvent = {value: string}
+
+/**
+ * Validates the event shape and converts the arguments of a StoreEvent.
+ *
+ * @param event whose content is expected to match a StoreEvent.
+ *              If expectation are not met, test will be failed.
+ */
+export function storeEvent(event: Event): ActualStoreEvent {
+    const store = event as StoreEvent
+    expect(store.args).is.not.undefined
+
+    const args = store.args
+    expect(args?.value).is.not.undefined
+
+    return args
+}
